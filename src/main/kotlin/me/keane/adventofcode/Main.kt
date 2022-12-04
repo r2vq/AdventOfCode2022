dcef3a4f121dc3738da52b221618a40880784a3a
Keane Quibilan <arv.r2vq@gmail.com>
2022-12-04 00:43:21 -0500

    Day 4 Question 2

--- src/main/kotlin/me/keane/adventofcode/Main.kt
+++ src/main/kotlin/me/keane/adventofcode/Main.kt
@@ -1,10 +1,10 @@
 package me.keane.adventofcode
 
-import me.keane.adventofcode.day4.question1
+import me.keane.adventofcode.day4.question2
 import java.io.File
 
 fun main(args: Array<String>) {
-    println("Advent of Code 2022 Day 4 Question 1")
+    println("Advent of Code 2022 Day 4 Question 2")
     val file = File("day4-input.txt")
-    println(question1(file))
+    println(question2(file))
 }
