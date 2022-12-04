b3aa192f4cd12b8803581fee69676e28d4bbf012
Keane Quibilan <arv.r2vq@gmail.com>
2022-12-04 00:39:17 -0500

    Day 4 Question 1

--- src/main/kotlin/me/keane/adventofcode/Main.kt
+++ src/main/kotlin/me/keane/adventofcode/Main.kt
@@ -1,10 +1,10 @@
 package me.keane.adventofcode
 
-import me.keane.adventofcode.day3.question2
+import me.keane.adventofcode.day4.question1
 import java.io.File
 
 fun main(args: Array<String>) {
-    println("Advent of Code 2022 Day 3 Question 2")
-    val file = File("day3-input.txt")
-    println(question2(file))
+    println("Advent of Code 2022 Day 4 Question 1")
+    val file = File("day4-input.txt")
+    println(question1(file))
 }
--- src/main/kotlin/me/keane/adventofcode/day4/Question1.kt
+++ src/main/kotlin/me/keane/adventofcode/day4/Question1.kt
@@ -1,2 +1,25 @@
 package me.keane.adventofcode.day4
 
+import me.keane.adventofcode.util.map
+import java.io.File
+
+fun question1(file: File): Int = file
+    .map { line ->
+        line
+            .split(",")
+            .map { stringRange -> stringRange.split("-").map { num -> num.toInt() } }
+            .map { range -> range[0] to range[1] }
+    }
+    .map { (first, second) ->
+        if (first.start > second.start || first.start == second.start && first.end < second.end) {
+            second to first
+        } else {
+            first to second
+        }
+    }
+    .filter { (first, second) -> first.end >= second.start && second.end <= first.end }
+    .count()
+    .toInt()
+
+private val Pair<Int, Int>.start get() = first
+private val Pair<Int, Int>.end get() = second
