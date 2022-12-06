e8c0e3d084c4325bb40c59be91f3660eb05ee3eb
Keane Quibilan <arv.r2vq@gmail.com>
2022-12-06 00:25:39 -0500

    Day 6 Question 2

--- src/main/kotlin/me/keane/adventofcode/Main.kt
+++ src/main/kotlin/me/keane/adventofcode/Main.kt
@@ -1,10 +1,10 @@
 package me.keane.adventofcode
 
-import me.keane.adventofcode.day6.question1
+import me.keane.adventofcode.day6.question2
 import java.io.File
 
 fun main(args: Array<String>) {
-    println("Advent of Code 2022 Day 6 Question 1")
+    println("Advent of Code 2022 Day 6 Question 2")
     val file = File("day6-input.txt")
-    println(question1(file))
+    println(question2(file))
 }
---
+++ src/main/kotlin/me/keane/adventofcode/day6/Question2.kt
@@ -0,0 +1,34 @@
+package me.keane.adventofcode.day6
+
+import java.io.File
+import java.util.*
+
+private const val bufferSize = 14
+
+fun question2(file: File): Int {
+    val line = file.readLines().first()
+    val buffer = LinkedList<Char>()
+    line.forEachIndexed { index, c ->
+        if (buffer.size < bufferSize) {
+            buffer.add(c)
+        } else {
+            buffer.pop()
+            buffer.add(c)
+        }
+        if (buffer.size == bufferSize && buffer.checkUnique()) {
+            return index + 1
+        }
+    }
+    return -1
+}
+
+private fun LinkedList<Char>.checkUnique(): Boolean {
+    for (i in 0 until size - 1) {
+        for (j in i + 1 until size) {
+            if (get(i) == get(j)) {
+                return false
+            }
+        }
+    }
+    return true
+}
